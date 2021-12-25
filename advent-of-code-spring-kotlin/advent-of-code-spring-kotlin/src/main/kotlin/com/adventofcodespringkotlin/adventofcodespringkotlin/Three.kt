c051e066ed1a575a3a71fc8179820730324a82c4
Joseph Rousoss <jroussos01@gmail.com>
2021-12-25 01:55:38 -0500

    day three part 1 completed

--- advent-of-code-spring-kotlin/advent-of-code-spring-kotlin/src/main/kotlin/com/adventofcodespringkotlin/adventofcodespringkotlin/Three.kt
+++ advent-of-code-spring-kotlin/advent-of-code-spring-kotlin/src/main/kotlin/com/adventofcodespringkotlin/adventofcodespringkotlin/Three.kt
@@ -1,28 +1,103 @@
 package com.adventofcodespringkotlin.adventofcodespringkotlin
 
+import java.io.File
+
 class Three {
 
-    fun binaryDecoder(input: List<String>): Int {
+    fun ListMaker(input: String): List<String> {
+        val inputFile = input
+        val lines: List<String> = File(inputFile).readLines()
+        //lines.forEach {line -> println(line) }
+        return lines
+    }
 
-        for (x in input) {
-            // index through a string
-            println(x)
-        }
-        return 0
+    fun binaryDecoder(input: String): Int {
+        //var binaryNum = input.toLong() not needed anymore
+        return input.toInt(2)
     }
 
     fun gammaDecoder(input: List<String>): String {
-        var index1_ones = 0
-        var index1_twos = 0
+        var strLength = 11
+        var numZeros_last = 0
+        var numOnes_last = 0
+        var numZeros = 0
+        var numOnes = 0
         var temp: String
-        for (x in input) {
-            temp = x.get(0).toString()
-            println(temp)
+        var final = "" // defaulting to 0
+        var j = 0
+        for (i in 0..strLength) { // this is the length of each string
+            for (x in input) {
+                temp = x.get(i).toString() // gets the number for each column
+                if (temp.equals("0")) {
+                    ++numZeros
+                }
+                else {
+                    ++numOnes
+                }
+                if (i > j) {
+                    //println("new column")
+                    // determine most then add to string
+                    if (numOnes > numZeros) {
+                        final += "1"
+                    }
+                    else {
+                        final += "0"
+                    }
+                    // reset numZeros and numOnes
+                    numZeros = 0
+                    numOnes = 0
+                    ++j
+                }
 
+                if (i == strLength) {
+                    if (temp.equals("0")) {
+                        ++numZeros_last
+                    }
+                    else {
+                        ++numOnes_last
+                    }
+                }
+
+            }
+        }
+        // for the very last column otherwise missed
+        if (numOnes_last > numZeros_last) {
+            final += "1"
+        }
+        else {
+            final += "0"
+        }
+        return final
+    }
+
+    fun epsilonDecoder(input: String): String {
+        val strList:  MutableList<Char> = mutableListOf<Char>()
+        for (x in input) {
+            strList.add(x)
+        }
+        for (i in strList.indices) {
+            if (strList[i].equals('0')) {
+                strList[i] = '1'
+            }
+            else {
+                strList[i] = '0'
+            }
         }
 
-        return "null"
+        return String(strList.toCharArray())
     }
+
+    fun powerConsumption(input: List<String>): Int {
+        var gammaBinary = gammaDecoder(input)
+        var epsilonBinary = epsilonDecoder(gammaBinary)
+        var gammaInt = binaryDecoder(gammaBinary)
+        var epsilonInt = binaryDecoder(epsilonBinary)
+
+        return gammaInt * epsilonInt
+    }
+
+
+
 }
 
 
--- advent-of-code-spring-kotlin/advent-of-code-spring-kotlin/src/test/kotlin/com/adventofcodespringkotlin/adventofcodespringkotlin/ThreeTest.kt
+++ advent-of-code-spring-kotlin/advent-of-code-spring-kotlin/src/test/kotlin/com/adventofcodespringkotlin/adventofcodespringkotlin/ThreeTest.kt
@@ -10,19 +10,8 @@ internal class ThreeTest {
 
     @Test
     fun binaryDecoderTest() {
-        val  input:  List<String> = listOf("00100",
-            "11110",
-            "10110",
-            "10111",
-            "10101",
-            "01111",
-            "00111",
-            "11100",
-            "10000",
-            "11001",
-            "00010",
-            "01010")
-        val expected = 0 // will be 198 later
+        val  input = "01001"
+        val expected = 9
         val actual = three.binaryDecoder(input)
         assertEquals(expected, actual)
     }
@@ -41,8 +30,83 @@ internal class ThreeTest {
             "11001",
             "00010",
             "01010")
-        val expected = "null" // will be 198 later
+        val expected = "10110"// 10110 is correct or 22
         val actual = three.gammaDecoder(input)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun epsilonDecoderTest() {
+        val  input:  List<String> = listOf("00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010")
+        val expected = "0110"// 01001 is correct or 9
+        val actual = three.epsilonDecoder("1001")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun powerConsumption() {
+        val  input:  List<String> = listOf("00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010")
+        val expected = 198
+        val actual = three.powerConsumption(input)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun listMakerTest() {
+        val input = "C:\\Users\\Jrous\\repositories\\advent-of-code-cheeseycube\\advent-of-code-spring-kotlin\\advent-of-code-spring-kotlin\\Input3"
+        val expected = listOf("11001", "00010")
+        val actual = three.ListMaker(input)
+        assertEquals(expected, actual)
+        // this is expected to fail
+    }
+
+    /*@Test
+    fun gammaDecoderTestFinal() {
+        val  input: List<String> = three.ListMaker("C:\\Users\\Jrous\\repositories\\advent-of-code-cheeseycube\\advent-of-code-spring-kotlin\\advent-of-code-spring-kotlin\\Input3")
+        val expected = "10110"// 10110 is correct or 22
+        val actual = three.gammaDecoder(input)
+        assertEquals(expected, actual)
+    }*/
+
+    @Test
+    fun powerConsumptionTestFinal() {
+        val  input: List<String> = three.ListMaker("C:\\Users\\Jrous\\repositories\\advent-of-code-cheeseycube\\advent-of-code-spring-kotlin\\advent-of-code-spring-kotlin\\Input3")
+        /*val  input:  List<String> = listOf("00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",)*/
+        val expected = 1131506 // my answer, 1131506
+        val actual = three.powerConsumption(input)
         assertEquals(expected, actual)
     }
 }
