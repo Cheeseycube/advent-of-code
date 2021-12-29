aff1fe45e849d89a20e0f04400a0780ddd6c2a5b
Joseph Rousoss <jroussos01@gmail.com>
2021-12-29 03:03:19 -0500

    day three part 2 completed

--- advent-of-code-spring-kotlin/advent-of-code-spring-kotlin/src/main/kotlin/com/adventofcodespringkotlin/adventofcodespringkotlin/Three.kt
+++ advent-of-code-spring-kotlin/advent-of-code-spring-kotlin/src/main/kotlin/com/adventofcodespringkotlin/adventofcodespringkotlin/Three.kt
@@ -12,7 +12,6 @@ class Three {
     }
 
     fun binaryDecoder(input: String): Int {
-        //var binaryNum = input.toLong() not needed anymore
         return input.toInt(2)
     }
 
@@ -27,7 +26,7 @@ class Three {
         var j = 0
         for (i in 0..strLength) { // this is the length of each string
             for (x in input) {
-                temp = x.get(i).toString() // gets the number for each column
+                temp = x.get(i).toString() // gets the bit for each column
                 if (temp.equals("0")) {
                     ++numZeros
                 }
@@ -84,7 +83,7 @@ class Three {
             }
         }
 
-        return String(strList.toCharArray())
+        return String(strList.toCharArray()) // converting a list to a string
     }
 
     fun powerConsumption(input: List<String>): Int {
@@ -96,10 +95,300 @@ class Three {
         return gammaInt * epsilonInt
     }
 
+// to find oxygen generator rating: look at first column of bits,
+// and find the most common value, kind of like what we did before
+// now remove any sets of bits that don't have that common value in
+// the given position.  rinse and repeat on each column until only one left
+// also if equal just pretend that 1 is the most common
 
+    fun oxygenRating(input: List<String>, strLength: Int): String {
+        val strList = input.toMutableList()
+        var numZeros_last = 0
+        var numOnes_last = 0
+        var numZeros = 0
+        var numOnes = 0
+        var temp: String
+        var j = 0
+        for (i in 0..strLength) { // this is the length of each string
+            // short circuiting if done
+            if (strList.size < 2) {
+                return strList.joinToString() // optional separator could be used here for future reference
+            }
+            for (x in strList.reversed()) {
+                temp = x.get(i).toString() // gets the bit for each column
+                if (temp.equals("0")) {
+                    ++numZeros
+                }
+                else {
+                    ++numOnes
+                }
+                if (i > j) {
+                    // determine most common
+                    if (numOnes >= numZeros) {
+                        // checking every string at only the current index
+                        // then deleting from the list as needed
+                        for (c in strList.indices.reversed()) {
+                            temp = strList[c].get(j).toString()
+                            if (temp.equals("0")) {
+                                strList.removeAt(c)
+                            }
+                        }
+                    }
+                    else {
+                        for (c in strList.indices.reversed()) {
+                            temp = strList[c].get(j).toString()  // was x.get
+                            if (temp.equals("1")) {
+                                strList.removeAt(c)
+                                //println("removed")
+                            }
+                        }
+                    }
+                    // reset numZeros and numOnes
+                    numZeros = 0
+                    numOnes = 0
+                    ++j
+                }
+                //return strList
+
+                if (i == strLength) {
+                    if (temp.equals("0")) {
+                        ++numZeros_last
+                    }
+                    else {
+                        ++numOnes_last
+                    }
+                }
+
+            }
+        }
+        // for the very last column otherwise missed
+        if (numOnes_last >= numZeros_last) {
+            for (c in strList.indices.reversed()) {
+                temp = strList[c].get(j).toString()  // was x.get
+                if (temp.equals("0")) {
+                    strList.removeAt(c)
+                    //println("removed")
+                }
+            }
+        }
+        else {
+            for (c in strList.indices.reversed()) {
+                temp = strList[c].get(j).toString()  // was x.get
+                if (temp.equals("1")) {
+                    strList.removeAt(c)
+                    //println("removed")
+                }
+            }
+        }
+        return strList.joinToString()
+    }
+
+
+    fun co2Rating(input: List<String>, strLength: Int): String {
+        val strList = input.toMutableList()
+        var numZeros_last = 0
+        var numOnes_last = 0
+        var numZeros = 0
+        var numOnes = 0
+        var temp: String
+        var j = 0
+        for (i in 0..strLength) { // this is the length of each string
+            // short circuiting if done
+            if (strList.size < 2) {
+                return strList.joinToString()
+            }
+            for (x in strList.reversed()) {  // reversed did not work so well
+                temp = x.get(i).toString() // gets the bit for each column
+                if (temp.equals("0")) {
+                    ++numZeros
+                }
+                else {
+                    ++numOnes
+                }
+                if (i > j) {
+                    // determine most common
+                    if (numZeros > numOnes) {
+                        // checking every string at only the current index
+                        // then deleting from the list as needed
+                        for (x in strList.reversed()) {
+                            temp = x.get(j).toString()
+                            if (temp.equals("0")) {
+                                if (strList.size < 2) {  // hotfix
+                                    return strList.joinToString()
+                                }
+                                strList.remove(x)
+                                //return(strList)
+                            }
+                        }
+                    }
+                    else {
+                        for (x in strList.reversed()) {
+                            temp = x.get(j).toString()
+                            if (temp.equals("1")) {
+                                if (strList.size < 2) {  // hotfix
+                                    return strList.joinToString()
+                                }
+                                strList.remove(x)
+                            }
+                        }
+                    }
+                    // reset numZeros and numOnes
+                    numZeros = 0
+                    numOnes = 0
+                    ++j
+                }
+
+                if (i == strLength) {
+                    if (temp.equals("0")) {
+                        ++numZeros_last
+                    }
+                    else {
+                        ++numOnes_last
+                    }
+                }
+
+            }
+        }
+        // for the very last column otherwise missed
+        if (numZeros_last > numOnes_last) {
+            for (x in strList.reversed()) {
+                temp = x.get(j).toString()  // was x.get
+                if (temp.equals("0")) {
+                    if (strList.size < 2) {  // hotfix
+                        return strList.joinToString()
+                    }
+                    strList.remove(x)
+                    //println("removed")
+                }
+            }
+        }
+        else {
+            for (x in strList.reversed()) {
+                temp = x.get(j).toString()  // was x.get
+                if (temp.equals("1")) {
+                    if (strList.size < 2) {  // hotfix
+                        return strList.joinToString()
+                    }
+                    strList.remove(x)
+                    //println("removed")
+                }
+            }
+        }
+        return strList.joinToString()
+    }
+
+    fun lifeSupportRating(input: List<String>, strLength: Int): Int {
+        var oxygenRating = binaryDecoder(oxygenRating(input, strLength))
+        var co2Rating = binaryDecoder(co2Rating(input, strLength))
+
+        return oxygenRating * co2Rating
+    }
+
+
+
+
+    fun betteroxygenRating(input: List<String>, strLength: Int, i: Int): String {
+        val strList = input.toMutableList()
+        var temp: String
+        var numZeros = 0
+        var numOnes = 0
+        /*if (Iterator > strLength) {
+            return strList
+        }*/
+        for (x in strList) {
+            temp = x.get(i).toString()
+            if (temp.equals("0")) {
+                ++numZeros
+            }
+            else {
+                ++numOnes
+            }
+        }
+
+        if (numOnes >= numZeros) {
+            for (x in strList.reversed()) {
+                temp = x.get(i).toString()
+                if (temp.equals("0")) {
+                    strList.remove(x)
+                }
+            }
+        }
+        else {
+            for (x in strList.reversed()) {
+                temp = x.get(i).toString()
+                if (temp.equals("1")) {
+                    strList.remove(x)
+                }
+            }
+        }
+        //println(strList)
+        if (strList.size < 2) {
+            //println(strList)
+            return strList.joinToString()
+        }
+        /*else {
+            betteroxygenRating(strList, strLength, i + 1)
+        }*/
+        return betteroxygenRating(strList, strLength, i + 1)
+    }
+
+    fun betterco2Rating(input: List<String>, strLength: Int, i: Int): String {
+        val strList = input.toMutableList()
+        var temp: String
+        var numZeros = 0
+        var numOnes = 0
+        /*if (i > strLength) {
+            return strList.joinToString()
+        }*/
+        for (x in strList) {
+            temp = x.get(i).toString()
+            if (temp.equals("0")) {
+                ++numZeros
+            }
+            else {
+                ++numOnes
+            }
+        }
+
+        if (numZeros <= numOnes) {
+            for (x in strList.reversed()) {
+                temp = x.get(i).toString()
+                if (temp.equals("1")) {
+                    strList.remove(x)
+                }
+            }
+        }
+        else {
+            for (x in strList.reversed()) {
+                temp = x.get(i).toString()
+                if (temp.equals("0")) {
+                    strList.remove(x)
+                }
+            }
+        }
+        //println(strList)
+        if (strList.size < 2) {
+            println(strList)
+            return strList.joinToString()
+        }
+
+        return betterco2Rating(strList, strLength, i + 1)
+    }
+
+    fun betterlifeSupportRating(input: List<String>, strLength: Int): Int {
+        var oxygenRating = binaryDecoder(betteroxygenRating(input, strLength, 0))
+        var co2Rating = binaryDecoder(betterco2Rating(input, strLength, 0))
+
+        return oxygenRating * co2Rating
+    }
 
 }
 
 
+// ok ok new plan it turns out removing elements from an array while iterating is bad
+// so instead we find the most common for a specific index, starting with the very first
+// then we of course remove accordingly
+// THEN instead of trying to do it again in the same function, we call ANOTHER function whose only job
+// is to give me a new iterator, and we of course just run our own function again. ez clap
 
-// the plan: look at first index, see if it is 0 or 1 and add to a running total
+
--- advent-of-code-spring-kotlin/advent-of-code-spring-kotlin/src/test/kotlin/com/adventofcodespringkotlin/adventofcodespringkotlin/ThreeTest.kt
+++ advent-of-code-spring-kotlin/advent-of-code-spring-kotlin/src/test/kotlin/com/adventofcodespringkotlin/adventofcodespringkotlin/ThreeTest.kt
@@ -109,4 +109,167 @@ internal class ThreeTest {
         val actual = three.powerConsumption(input)
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun oxygenRatingTest() {
+        val  input:  List<String> = listOf("00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",)
+        val expected = "10111"// 10111 is correct or 23
+        val actual = three.oxygenRating(input, 4)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun oxygenRatingTestFinal() {
+        val  input: List<String> = three.ListMaker("C:\\Users\\Jrous\\repositories\\advent-of-code-cheeseycube\\advent-of-code-spring-kotlin\\advent-of-code-spring-kotlin\\Input3")
+        val expected = "111101011111"// this might be wrong
+        val actual = three.oxygenRating(input, 11)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun co2RatingTest() {
+        val  input:  List<String> = listOf("00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",)
+        val expected = "01010"// 01010 is correct or 10
+        val actual = three.co2Rating(input, 4)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun co2RatingTestFinal() {
+        val  input: List<String> = three.ListMaker("C:\\Users\\Jrous\\repositories\\advent-of-code-cheeseycube\\advent-of-code-spring-kotlin\\advent-of-code-spring-kotlin\\Input3")
+        val expected = listOf("10111")// 10111 is correct or 23
+        val actual = three.co2Rating(input, 11)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun lifeSupportRatingTest() {
+        val  input:  List<String> = listOf("00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010")
+        val expected = 230
+        val actual = three.lifeSupportRating(input, 4)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun lifeSupportRatingTestFinal() {
+        val  input: List<String> = three.ListMaker("C:\\Users\\Jrous\\repositories\\advent-of-code-cheeseycube\\advent-of-code-spring-kotlin\\advent-of-code-spring-kotlin\\Input3")
+        val expected = 236100// best guess so far is 236100 which is too low
+        val actual = three.lifeSupportRating(input, 11)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun betteroxygenRatingTest() {
+        val  input:  List<String> = listOf("00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",)
+        val expected = listOf("10111")// 10111 is correct or 23
+        val actual = three.betteroxygenRating(input, 4, 0)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun betteroxygenRatingTestFinal() {
+        val  input: List<String> = three.ListMaker("C:\\Users\\Jrous\\repositories\\advent-of-code-cheeseycube\\advent-of-code-spring-kotlin\\advent-of-code-spring-kotlin\\Input3")
+        val expected = listOf("111111111001")// 111111111001 different than other method
+        val actual = three.betteroxygenRating(input, 11, 0)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun betterco2RatingTest() {
+        val  input:  List<String> = listOf("00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",)
+        val expected = "01010"// 01010 is correct or 10
+        val actual = three.betterco2Rating(input, 4, 0)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun betterco2RatingTestFinal() {
+        val  input: List<String> = three.ListMaker("C:\\Users\\Jrous\\repositories\\advent-of-code-cheeseycube\\advent-of-code-spring-kotlin\\advent-of-code-spring-kotlin\\Input3")
+        val expected = "011110000011"// new answer 011110000011
+        val actual = three.betterco2Rating(input, 11, 0)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun betterlifeSupportRatingTest() {
+        val  input:  List<String> = listOf("00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010")
+        val expected = 230
+        val actual = three.betterlifeSupportRating(input, 4)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun betterlifeSupportRatingTestFinal() {
+        val  input: List<String> = three.ListMaker("C:\\Users\\Jrous\\repositories\\advent-of-code-cheeseycube\\advent-of-code-spring-kotlin\\advent-of-code-spring-kotlin\\Input3")
+        val expected = 7863147 // best guess so far is 236100 which is too low
+        val actual = three.betterlifeSupportRating(input, 11)
+        assertEquals(expected, actual)
+    }
 }
+
